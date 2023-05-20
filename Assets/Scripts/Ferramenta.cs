7cd21003be66c3155a14dc9a6b66d5e544462fdb
Gustavo <gustavofelix01@yahoo.com>
2023-05-20 18:04:44 -0300

    Update Ferramenta.cs

--- Assets/Scripts/Ferramenta.cs
+++ Assets/Scripts/Ferramenta.cs
@@ -6,6 +6,7 @@ public class Ferramenta : MonoBehaviour
 {
     private SpriteRenderer spriteRenderer;
     public Sprite newSprite;
+    public GameObject alimentoPronto;
     
     // Start is called before the first frame update
     void Start()
@@ -22,7 +23,25 @@ public class Ferramenta : MonoBehaviour
         {
             Destroy(collision.gameObject);
             spriteRenderer.sprite = newSprite; 
+            StartCoroutine(FritaAlimento());
         }
+    }
+
+    IEnumerator FritaAlimento()
+    {
+        //Print the time of when the function is first called. color red
+        Debug.Log("Started Coroutine at timestamp : " + Time.time);
+
+        //yield on a new YieldInstruction that waits for 5 seconds.
+        yield return new WaitForSeconds(0.5f);
+
+        Instantiate(alimentoPronto, transform.position, Quaternion.identity);
+
+        //After we have waited seconds print the time again. color blue
+        Debug.Log("Finished Coroutine at timestamp : " + Time.time);
+        
+        yield return new WaitForSeconds(0.5f);
+        
     }
     
 }
