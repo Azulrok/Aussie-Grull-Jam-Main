26e630621c51bebc12948e59f193e8294cc70d74
Gustavo <gustavofelix01@yahoo.com>
2023-05-20 17:49:45 -0300

    Criando prefabs

--- Assets/Scripts/Ferramenta.cs
+++ Assets/Scripts/Ferramenta.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Ferramenta : MonoBehaviour
 {
+    private SpriteRenderer spriteRenderer;
+    public Sprite newSprite;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        // print(transform.position);
     }
 
-    // Update is called once per frame
-    void Update()
+
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        print(collision.gameObject);
+        if (gameObject.tag == "Fritadeira" && collision.gameObject.tag == "frango")
+        {
+            Destroy(collision.gameObject);
+            spriteRenderer.sprite = newSprite; 
+        }
     }
+    
 }
