f34fb405ec8d16d0d228a050dfaeac5b7f8d0be3
Gustavo <gustavofelix01@yahoo.com>
2023-05-21 00:03:16 -0300

    script basico comanda

--- Assets/Principal/Scripts/Comandas.cs
+++ Assets/Principal/Scripts/Comandas.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Comandas : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Sprite frangoFrito;
+    public Sprite hamburguero;
+    public Sprite cookie;
+    public int id;
+
+    private Image spriteRenderer;
+
     void Start()
     {
-        
+        spriteRenderer = gameObject.GetComponent<Image>();
+        StartCoroutine(GeraCommanda());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator GeraCommanda()
     {
+        yield return new WaitForSeconds(1.5f);
         
+        if (id == 1)
+            spriteRenderer.sprite = frangoFrito;
+
+        yield return new WaitForSeconds(7f);
+
+        if (id == 2)
+            spriteRenderer.sprite = hamburguero;
+
+        yield return new WaitForSeconds(15f);
+        
+        if (id == 3)
+            spriteRenderer.sprite = cookie;      
     }
+
 }
